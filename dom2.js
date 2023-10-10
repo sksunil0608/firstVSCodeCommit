46fc2c325feeab7f13373b1ff49d71fec96c3393
sksunil0608 <sksunil0608@gmail.com>
2023-10-10 11:22:37 +0530

    get element by class name

---
+++ dom2.js
@@ -0,0 +1,16 @@
+// GET ELEMENT BY CLASSS NAME
+var items = document.getElementsByClassName('list-group-item');
+console.log(items)
+console.log(items[1])
+
+items[1].textContent = 'Hello 2'
+items[1].style.fontWeight ="bold";
+items[1].style.backgroundColor ="yellow";
+//We simply can't do this directly
+// items.style.backgroundColor="#fffff"
+
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor="grey"
+    items[i].style.fontWeight ="bold";
+}
+items[2].style.backgroundColor ='green';
