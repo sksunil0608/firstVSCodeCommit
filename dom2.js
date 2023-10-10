d7ecf9393471d691f0fcfad7c49dc72c33256a87
sksunil0608 <sksunil0608@gmail.com>
2023-10-10 13:31:57 +0530

    query selector and query selector all

--- dom2.js
+++ dom2.js
@@ -85,3 +85,7 @@ for (var i=0;i<odd.length;i++){
     odd[i].style.backgroundColor ="green";
 }
 
+var even = document.querySelectorAll('li:nth-child(even)');
+for (var i=0;i<even.length;i++){
+    even[i].style.backgroundColor ="red";
+}
