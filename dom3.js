78f8237162d99a49a38c5b264f68c8ec2a72c554
sksunil0608 <sksunil0608@gmail.com>
2023-10-12 14:33:48 +0530

    Search using Itemname and Description Name

--- dom3.js
+++ dom3.js
@@ -1,26 +1,60 @@
-var from = document.getElementById('addForm');
+var form = document.getElementById('addForm');
 
 var itemList = document.getElementById('items')
 
-//From Submit Event
-from.addEventListener('submit',addItem);
+
+// Add Edit button in the previous Elements
+var allItems = document.querySelectorAll('.list-group-item')
+
+for(let i = 0;i<allItems.length;i++){
+    let editBtn = document.createElement('button');
+    editBtn.className ='btn btn-info btn-sm float-right';
+    editBtn.style.marginRight = "2%";
+    editBtn.appendChild(document.createTextNode("Edit"));
+    allItems[i].appendChild(editBtn)
+}
+
+
+// Add description box
+let descBox = document.createElement('input');
+descBox.className = "form-control mr-2"
+descBox.id="desc";
+descBox.type ="text";
+
+form.insertBefore(descBox,document.getElementById('submit'))
+
+
+var filter = document.getElementById('filter');
+//form Submit Event
+form.addEventListener('submit',addItem);
 
 //Delete Event
 itemList.addEventListener('click',removeItem);
+
+//Filter Event
+filter.addEventListener('keyup',filterItems);
+
+
 function addItem(e){
     e.preventDefault();
 
     // Delete Event 
     //Get Input Value
     var newItem = document.getElementById('item').value;
+    //get desc
+    var newDesc = document.getElementById('desc').value; 
 
+    
     //Create new li Element
     var li =document.createElement('li');
     //Add class name to id
     li.className ='list-group-item';
-    
+
     //Add Text Node with Input Value
+    // newContent = `${newItem} ${newDesc}`
+    // li.appendChild(document.createTextNode(newContent));
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newDesc));
     
     //Delete Button Element
     var deleteBtn = document.createElement('button');
@@ -53,11 +87,33 @@ function EditItem(li,deleteBtn){
 
     let editBtn = document.createElement('button');
     editBtn.className ='btn btn-info btn-sm float-right';
-    editBtn.style.marginLeft = "2%";
-
+    editBtn.style.marginRight = "2%";
     editBtn.appendChild(document.createTextNode("Edit"));
 
     // deleteBtn.insertAdjacentElement('afterend', editBtn);
-    li.insertBefore(editBtn,deleteBtn)
+    // li.insertBefore(editBtn,deleteBtn)
+    li.appendChild(editBtn)
 
 }
+
+//Filter items
+
+function filterItems(e){
+    //convert text tp lowercase
+    var text = e.target.value.toLowerCase();
+    items = itemList.getElementsByTagName('li');
+    
+    Array.from(items).forEach(function(item){
+        var itemName1 = item.firstChild.textContent;
+        var itemName2 = item.firstChild.nextSibling.textContent;
+        itemName = itemName1 + itemName2
+        if (itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display ='block'
+        }
+        else{
+            item.style.display ="none";
+        }
+    });
+
+}
+
